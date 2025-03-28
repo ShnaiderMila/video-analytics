5493bf0a7ea51297e7bbfcc2fff7fa4bb169b5a6
Mila Shnaider <shnaiderln@mail.ru>
2025-03-28 14:26:41 +0300

    Initial commit: setup project with Vite, TypeScript, Redux, Saga

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Test task for frontend developer at Netris</h1>
+    </div>
+  );
+}
+
+export default App;
